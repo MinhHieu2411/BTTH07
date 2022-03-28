9fc826a1b800588fab66cc242d6a758a6f4e7389
phamthuhang123h <hengthupham@gmail.com>
2022-03-29 00:06:12 +0700

    Hang up

---
+++ js/form.js
@@ -0,0 +1,10 @@
+let IdPattern =  /^[a-z0-9]{5,12}$/
+
+let Id = document.getElementById('Id')
+let userError = document.getElementById('userError')
+let submit = document.getElementById('btnCheck')
+
+let txtPassword =  /^[a-z0-9]{5,12}$/
+
+
+

6a25b72bde944d6008340f57b15fc4d50ca86ce2
NgQuangMinh <quangminh29502@gmail.com>
2022-03-29 00:59:09 +0700

    Qminh up file

--- js/form.js
+++ js/form.js
@@ -1,10 +1,37 @@
-let IdPattern =  /^[a-z0-9]{5,12}$/
 
-let Id = document.getElementById('Id')
+let userId =  /^[a-z0-9]{5,12}$/
+
+
+let txtUser = document.getElementById('txtUser')
 let userError = document.getElementById('userError')
 let submit = document.getElementById('btnCheck')
 
-let txtPassword =  /^[a-z0-9]{5,12}$/
+submit.addEventListener('click',function(e){
+    e.preventDefault();
+    if(userId.test(txtUser.value)){
+        userError.textContent = 'ID hợp lệ'
+        userError.style.color = 'green'
+    }
+    else{
+        userError.textContent = 'ID không hợp lệ'
+        userError.style.color = 'red'
+    }
+})
 
+let passWord =  /^[a-z0-9]{5,12}$/
 
+let txtPassword = document.getElementById('txtPassword')
+let passError = document.getElementById('passError')
+
+submit.addEventListener('click',function(e){
+    e.preventDefault();
+    if(passWord.test(txtPass.value)){
+        passError.textContent = 'Mật khẩu mạnh'
+        passError.style.color = 'green'
+    }
+    else{
+        passError.textContent = 'Nhập lại mật khẩu'
+        passError.style.color = 'red'
+    }
+})
 
